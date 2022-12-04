ff91adec3f04b14f208fc7f2522246c526a184f4
spectrum912 <62601635+spectrum912@users.noreply.github.com>
2022-12-04 16:26:15 +0200

    K-SVD add

--- image_procesing_test.py
+++ image_procesing_test.py
@@ -11,7 +11,8 @@ from image_processing.noises import AWGN, ASCN, Mult, Speckle
 '''End'''
 '''For image filtering'''
 from image_processing.filters import DCT, Frost, Lee, Median
-from ksvd import ApproximateKSVD
+# from ksvd import ApproximateKSVD
+from image_processing.filters.KSVD import ksvd
 from image_processing.filters.BM3D_color import BM3D_1st_step_color, BM3D_2nd_step_color
 from scipy import ndimage
 
@@ -38,10 +39,11 @@ def metric(original, noised):
 
 if __name__ == "__main__":
     result_path = "test_result"
-    noise_type = "SPECKLE"
-    filter_type = "DCT"
+    noise_type = "AWGN"
+    filter_type = "KSVD"
 
-    image_loaded = cv2.imread("image_Lena512rgb.png")
+    image_path = "image_Lena512rgb.png"
+    image_loaded = cv2.imread(image_path)
 
     if noise_type == "AWGN":
         img_noise = AWGN.AWGN(image_loaded, 20)  # .astype(int)
@@ -70,7 +72,7 @@ if __name__ == "__main__":
         cv2.imwrite(f"{result_path}/{noise_type}_Final_sigma_color.png", cv2.cvtColor(Final_img, cv2.COLOR_YCrCb2BGR))
         print(metric(image_loaded, Final_img)[0])
 
-    elif filter_type == "DCT":   # ПАЛАМАЛАСЬ
+    elif filter_type == "DCT":
         print("DCT")
         img = DCT.dct_filter(img_noise, 100)
         cv2.imwrite(f"{result_path}/{noise_type}_DCT_color.png", img)
@@ -92,4 +94,10 @@ if __name__ == "__main__":
         print("Median")
         img = Median.median_filter(img_noise, 3)
         cv2.imwrite(f"{result_path}/{noise_type}_Median.png", img)
+        print(metric(image_loaded, img)[0])
+
+    elif filter_type == "KSVD":
+        print("K-SVD")
+        ksvd(f"{result_path}/{noise_type}_Noise.png", f"{result_path}/{noise_type}_K-SVD.png")
+        img = cv2.imread(f"{result_path}/{noise_type}_K-SVD.png")
         print(metric(image_loaded, img)[0])
